5ea634d9ce1c7bcd60eff79a494a534cc18fba07
Gabriel Azevedo <gsa384@gmail.com>
2022-01-13 10:29:25 -0300

    Projeto criado e Models implementados

---
+++ ControleFinanceiro.API/ControleFinanceiro.BLL/Models/Despesa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.BLL.Models
+{
+    public class Despesa
+    {
+        public int DespesaId { get; set; }
+        public int CartaoId { get; set; }
+        public Cartao Cartao { get; set; }
+        public string Descricao { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+        public double Valor { get; set; }
+        public int Day { get; set; }
+        public int MonthId { get; set; }
+        public Month Month { get; set; }
+        public int Year { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
---
+++ ControleFinanceiro.API/ControleFinanceiro.BLL/Models/Month.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.BLL.Models
+{
+    public class Month
+    {
+        public int MonthId { get; set; }
+        public string Nome { get; set; }
+        public virtual ICollection<Despesa> Despesas { get; set; }
+        public virtual ICollection<Ganho> Ganhos { get; set; }
+
+    }
+}
